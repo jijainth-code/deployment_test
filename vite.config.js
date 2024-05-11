e44606f84c0905cc474c2682a45e4a2e346d7ae6
jijainth-code <jijainth.dhinakaran@stud.th-deg.de>
2024-05-11 12:16:06 +0200

    big fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/test_deploy/",
+  base:"/deployment_test/",
   plugins: [react()],
 })
